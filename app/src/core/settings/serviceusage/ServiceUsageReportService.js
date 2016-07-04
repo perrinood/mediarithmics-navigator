4a4003c3ca0bcfba39f68ff060a03b8ab6df4632
Thomas Tosoni <ttosoni@mediarithmics.com>
2016-07-04 17:52:17 +0200

    Update Service Usage Page

---
+++ app/src/core/settings/serviceusage/ServiceUsageReportService.js
@@ -0,0 +1,117 @@
+define(['./module', 'lodash', 'core/common/ReportWrapper'], function (module, _, ReportWrapper) {
+  'use strict';
+
+  var tableHeaders = {
+    "provider_organisation_id": {name: "Provider Organisation Id", type: "number"},
+    "service_id": {name: "Service Id", type: "number"},
+    "service_element_id": {name: "Service Element Id", type: "number"},
+    "customer_organisation_id": {name: "Customer Organisation Id", type: "number"},
+    "campaign_id": {name: "Campaign Id", type: "number"},
+    "sub_campaign_id": {name: "Sub Campaign Id", type: "number"},
+    "unit_count": {name: "Unit Count", type: "number"}
+  };
+
+  /**
+   * Campaign Analytics Report Service
+   */
+  module.factory('ServiceUsageReportService', ['$resource', 'Restangular', 'core/common/auth/Session',
+    'core/common/auth/AuthenticationService', 'core/configuration', 'moment',
+    function ($resource, Restangular, Session, AuthenticationService, configuration, moment) {
+      var WS_URL = configuration.WS_URL;
+
+      var serviceUsageCustomerResource = $resource(
+        WS_URL + "/reports/service_usage_customer_report",
+        {},
+        {
+          get: {
+            method: 'GET',
+            headers: {'Authorization': AuthenticationService.getAccessToken()}
+          }
+        }
+      );
+
+      var serviceUsageProviderResource = $resource(
+        WS_URL + "/reports/service_usage_provider_report",
+        {},
+        {
+          get: {
+            method: 'GET',
+            headers: {'Authorization': AuthenticationService.getAccessToken()}
+          }
+        }
+      );
+
+      /**
+       * Default Date Range Used For Daily Stats
+       */
+
+      var range = {startDate: moment().subtract('days', 20), endDate: moment()};
+
+      var startDate = function () {
+        return moment(range.startDate).startOf('day');
+      };
+
+      var endDate = function () {
+        return moment(range.endDate).add(1, 'day').startOf('day');
+      };
+
+
+      /**
+       * Report Service
+       */
+
+      var ReportService = {};
+
+      ReportService.getTableHeaders = function () {
+        return tableHeaders;
+      };
+
+      ReportService.getPerformance = function (resource, dimensions, metrics, filters, sort, limit) {
+        return resource.get({
+          organisation_id: Session.getCurrentWorkspace().organisation_id,
+          start_date: startDate().format('YYYY-MM-D'),
+          end_date: endDate().format('YYYY-MM-D'),
+          dimension: dimensions,
+          metrics: metrics,
+          filters: filters,
+          sort: sort,
+          limit: limit || null
+        });
+      };
+
+      ReportService.buildPerformanceReport = function (resource, dimensions, metrics, filters, sort, limit) {
+        return this.getPerformance(resource, dimensions, metrics, filters, sort, limit)
+          .$promise.then(function (response) {
+            return new ReportWrapper(response.data.report_view, tableHeaders);
+          });
+      };
+
+      // TODO MAKE THE DECORATORS WORK!
+      ReportService.serviceUsageCustomerReport = function (organisationId, sort, limit) {
+        return this.buildPerformanceReport(
+          serviceUsageCustomerResource,
+          // "campaign_id,campaign_name,service_name,service_element_name,customer_name",
+          "campaign_id,service_id,service_element_id",
+          "unit_count",
+          "organisation_id==" + organisationId,
+          sort,
+          limit
+        );
+      };
+
+      // TODO MAKE THE DECORATORS WORK!
+      ReportService.serviceUsageProviderReport = function (organisationId, sort, limit) {
+        return this.buildPerformanceReport(
+          serviceUsageProviderResource,
+          // "campaign_id,campaign_name,service_name,service_element_name,provider_name",
+          "campaign_id,service_id,service_element_id",
+          "unit_count",
+          "organisation_id==" + organisationId,
+          sort,
+          limit
+        );
+      };
+
+      return ReportService;
+    }]);
+});
--- app/src/core/settings/serviceusage/ViewAllController.js
+++ app/src/core/settings/serviceusage/ViewAllController.js
@@ -1,14 +1,103 @@
-define(['./module'], function (module) {
+define(['./module', 'angular'], function (module, angular) {
   'use strict';
 
   module.controller('core/settings/serviceusage/ViewAllController', [
-    '$scope', '$log', '$location', '$state', '$stateParams', 'Restangular', 'core/common/auth/Session',
-    function ($scope, $log, $location, $state, $stateParams, Restangular, Session) {
+    '$scope', '$log', '$location', '$state', '$stateParams', 'Restangular', 'core/common/auth/Session', 'ServiceUsageReportService', 'lodash',
+    function ($scope, $log, $location, $state, $stateParams, Restangular, Session, ServiceUsageReportService, _) {
       $scope.datamartId = Session.getCurrentDatamartId();
       $scope.organisationId = Session.getCurrentWorkspace().organisation_id;
       $scope.itemsPerPage = 20;
       $scope.currentPage = 0;
       $scope.usages = [];
+
+      // TODO MAKE DECORATORS WORK IN ORDER TO STOP DOING THIS BEAUTIFUL MANUAL JOIN...
+      // Get campaign names
+      Restangular.all('campaigns').getList({organisation_id: $scope.organisationId}).then(function (campaignsData) {
+        $scope.campaignNames = [];
+        _.forEach(campaignsData, function (campaign) {
+          $scope.campaignNames[campaign.id] = campaign.name;
+        });
+      });
+
+      // Get services names
+      Restangular.all('services').getList({organisation_id: $scope.organisationId}).then(function (servicesData) {
+        $scope.serviceNames = [];
+        _.forEach(servicesData, function (service) {
+          $scope.serviceNames[service.id] = service.name;
+        });
+      });
+
+      // Get segments names for stats display
+      Restangular.all('audience_segments').getList({organisation_id: $scope.organisationId}).then(function (segmentsData) {
+        $scope.serviceElementNames = [];
+        _.forEach(segmentsData, function (segment) {
+          $scope.serviceElementNames[segment.id] = segment.name;
+        });
+      });
+
+      ServiceUsageReportService.serviceUsageCustomerReport($scope.organisationId).then(function (data) {
+        $scope.serviceUsageCustomerReport = data;
+      });
+
+      // ServiceUsageReportService.serviceUsageProviderReport($scope.organisationId).then(function (data) {
+      //   $scope.serviceUsageProviderReport = data;
+      // });
+
+      // var buildProviderServiceUsageReport = function (serviceUsage) {
+      //   var usages = [];
+      //
+      //   var usageRows = serviceUsage.getRows();
+      //   for (var i = 0; i < usageRows.length; ++i) {
+      //     usages[i] = {
+      //       provider_organisation_id: usageRows[i][1],
+      //       campaign_id: usageRows[i][1],
+      //       campaign_name: $scope.campaignNames[usageRows[i][1]],
+      //       service_name: $scope.serviceNames[usageRows[i][2]],
+      //       // Keep only stats values. There's only one, unit_count so wee keep the first
+      //       unit_count: usageRows[i][3]
+      //     }
+      //   }
+      //
+      //   return usages;
+      // };
+      //
+      // $scope.$watchGroup(['serviceUsageProviderReport', 'campaignNames', 'serviceNames'], function (values) {
+      //   var serviceUsageProviderReport = values[0];
+      //   var campaignNames = values[1];
+      //   var serviceNames = values[2];
+      //   if (angular.isDefined(serviceUsageProviderReport) && angular.isDefined(campaignNames) && angular.isDefined(serviceNames)) {
+      //     $scope.providerUsages = buildServiceUsageReport(serviceUsageProviderReport);
+      //     console.log($scope.providerUsages);
+      //   }
+      // });
+
+      var buildCustomerServiceUsageReport = function (serviceUsage) {
+        var usages = [];
+
+        var usageRows = serviceUsage.getRows();
+        for (var i = 0; i < usageRows.length; ++i) {
+          usages[i] = {
+            customer_organisation_id: usageRows[i][0],
+            campaign_name: $scope.campaignNames[usageRows[i][1]],
+            service_name: $scope.serviceNames[usageRows[i][2]],
+            service_element_name: $scope.serviceElementNames[usageRows[i][3]],
+            // Keep only stats values. There's only one, unit_count so wee keep the first
+            unit_count: usageRows[i][4]
+          };
+        }
+
+        return usages;
+      };
+
+      $scope.$watchGroup(['serviceUsageCustomerReport', 'campaignNames', 'serviceNames', 'serviceElementNames'], function (values) {
+        var serviceUsageCustomerReport = values[0];
+        var campaignNames = values[1];
+        var serviceNames = values[2];
+        var serviceElementNames = values[3];
+        if (angular.isDefined(serviceUsageCustomerReport) && angular.isDefined(campaignNames) && angular.isDefined(serviceNames) && angular.isDefined(serviceElementNames)) {
+          $scope.customerUsages = buildCustomerServiceUsageReport(serviceUsageCustomerReport);
+        }
+      });
     }
   ]);
 });
