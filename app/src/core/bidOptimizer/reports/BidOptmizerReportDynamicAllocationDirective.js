dc731dfd6c8eeee5ef66ba883232e8aa5cfffaea
rait <rait@mediarithmics.com>
2016-08-08 18:14:30 +0200

    Remove console.log

--- app/src/core/bidOptimizer/reports/BidOptmizerReportDynamicAllocationDirective.js
+++ app/src/core/bidOptimizer/reports/BidOptmizerReportDynamicAllocationDirective.js
@@ -285,27 +285,18 @@ define(['./module', 'angular'], function (module, angular) {
                   scope.selectedCampaign = scope.selectedEnvironment.campaign;
 
                   scope.sum = function(data, field){
-                  var filter = data.filter();
-                  var keys = Object.keys(filter);
+                    var filter = data.filter();
+                    var keys = Object.keys(filter);
 
-                  console.log('keys',keys);
+                    var filteredData = _.filter(scope.allocationsData, function(o){
+                      return  _.reduce(keys.map(function(key){
 
+                        return o[key].indexOf(filter[key]) > -1;
+                      }) , function(memo, num){ return memo && num; } , true);
+                    });
 
-                  var filteredData = _.filter(scope.allocationsData, function(o){
-                  return  _.reduce(keys.map(function(key){
-                                                          console.log('value',o[key]);
-                                                          console.log('filter value',filter[key]);
-
-                                                        return o[key].indexOf(filter[key]) > -1;
-                                                      }) , function(memo, num){ return memo && num; } , true);
-                  });
-
-
-                  console.log('filteredData',filteredData.length);
-
-
-                        return _.sum(filteredData, field);
-                      };
+                    return _.sum(filteredData, field);
+                  };
                   scope.refreshAdGroups();
                   changeAllocation(scope.selectedEnvironment);
                 });
