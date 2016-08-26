150640069280d0926e0a820e23487ef2d0589f6f
David Duponchel <dduponchel@mediarithmics.com>
2016-08-26 14:12:25 +0200

    Improve the service usage table.
    The old version only displayed 20 items. The new one paginates, sorts,
    filters, shows a loading gif, exports it.

--- app/src/core/settings/serviceusage/ViewAllController.js
+++ app/src/core/settings/serviceusage/ViewAllController.js
@@ -2,51 +2,85 @@ define(['./module'], function (module) {
   'use strict';
 
   module.controller('core/settings/serviceusage/ViewAllController', [
-    '$scope', '$log', '$location', '$state', '$stateParams', 'Restangular', 'core/common/auth/Session', 'ServiceUsageReportService', 'lodash', '$q',
-    function ($scope, $log, $location, $state, $stateParams, Restangular, Session, ServiceUsageReportService, _, $q) {
+    '$scope', '$log', '$location', '$state', '$stateParams', 'Restangular', 'core/common/auth/Session', 'ServiceUsageReportService', 'lodash', '$q', 'ngTableParams', 'core/common/files/ExportService',
+    function ($scope, $log, $location, $state, $stateParams, Restangular, Session, ServiceUsageReportService, _, $q, NgTableParams, ExportService) {
       var currentWorkspace = Session.getCurrentWorkspace();
       $scope.datamartId = Session.getCurrentDatamartId();
       $scope.organisationId = Session.getCurrentWorkspace().organisation_id;
-      $scope.itemsPerPage = 20;
-      $scope.currentPage = 0;
-      $scope.usages = [];
+
+      var fetchedUsages = null;
 
       $scope.reportDateRange = ServiceUsageReportService.getDateRange();
 
-      var updateStatistics = function ($scope, ServiceUsageReportService) {
+      function buildProviderServiceUsageReport(serviceUsage) {
+        var usageRows = serviceUsage.getRows();
+        var usages = usageRows.map(function (usageRow) {
+          return {
+            provider_organisation_id: usageRow[0],
+            provider_name: usageRow[1],
+            campaign_id: usageRow[2],
+            campaign_name: usageRow[3],
+            service_id: usageRow[4],
+            service_name: usageRow[5],
+            service_element_id: usageRow[6],
+            segment_name: usageRow[7],
+            unit_count: usageRow[8]
+          };
+        });
+
+        fetchedUsages = usages;
+        $scope.tableParams.settings({
+          data: usages
+        });
+      }
+
+      $scope.tableParams = new NgTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+      });
+
+      function updateStatistics($scope, ServiceUsageReportService) {
+        var currentStatObj = $scope.statisticsQuery = {
+          rand: Math.random().toString(36).substring(8),
+          isRunning: true,
+          error: null
+        };
         ServiceUsageReportService.setDateRange($scope.reportDateRange);
         ServiceUsageReportService.serviceUsageProviderReport($scope.organisationId).then(function (data) {
+          currentStatObj.isRunning = false;
+          if (currentStatObj.rand !== $scope.statisticsQuery.rand) {
+            return;
+          }
           buildProviderServiceUsageReport(data);
         });
-      };
+      }
 
       $scope.$watch('reportDateRange', function () {
         updateStatistics($scope, ServiceUsageReportService, currentWorkspace.organisation_id);
       });
 
-      // ServiceUsageReportService.serviceUsageCustomerReport($scope.organisationId).then(function (data) {
-      //   buildCustomerServiceUsageReport(data);
-      // });
-
-      ServiceUsageReportService.serviceUsageProviderReport($scope.organisationId).then(function (data) {
-        buildProviderServiceUsageReport(data);
-      });
-
-      var buildProviderServiceUsageReport = function (serviceUsage) {
-        var usageRows = serviceUsage.getRows();
-        for (var i = 0; i < usageRows.length; ++i) {
-          $scope.usages[i] = {
-            provider_organisation_id: usageRows[i][0],
-            provider_name: usageRows[i][1],
-            campaign_id: usageRows[i][2],
-            campaign_name: usageRows[i][3],
-            service_id: usageRows[i][4],
-            service_name: usageRows[i][5],
-            service_element_id: usageRows[i][6],
-            segment_name: usageRows[i][7],
-            unit_count: usageRows[i][8]
-          };
+      $scope.export = function (extension) {
+        // TODO handle different types
+        if (extension !== 'xlsx') {
+          return;
         }
+
+        ExportService.exportData([{
+          name: "Service Usage Report",
+          data: [
+            ['All service usage reports'],
+            ["From " + $scope.reportDateRange.startDate.format("DD-MM-YYYY"), "To " + $scope.reportDateRange.endDate.format("DD-MM-YYYY")],
+            ['Provider Name', 'Campaign Name', 'Service Name', 'Service Element Name', 'Usage']
+          ].concat(fetchedUsages.map(function(usage) {
+            return [
+              usage.provider_name,
+              usage.campaign_name,
+              usage.service_name,
+              usage.segment_name,
+              usage.unit_count
+            ];
+          }))
+        }], 'Service Usage Report', extension);
       };
     }
   ]);
