25ba10f5cce5abc836e3d9f00b0e07f5d1208ac1
Nicolas Dussart <ndussart@mediarithmics.com>
2016-09-29 18:12:13 +0200

    Add partition statistics

--- app/src/core/datamart/partitions/EditAllController.js
+++ app/src/core/datamart/partitions/EditAllController.js
@@ -16,30 +16,6 @@ define(['./module'], function (module) {
         $scope.sortType = 'name';
       });
 
-      // $scope.reportDateRange = {startDate: moment(), endDate: moment()};
-      // var todayDate = {startDate: moment(), endDate: moment()};
-      // var sevenDaysRange = {startDate: moment().subtract('days', 7), endDate: moment()};
-      // AudienceSegmentAnalyticsReportService.setDateRange(todayDate);
-
-      // updateStatistics($scope, AudienceSegmentAnalyticsReportService);
-      // $scope.refresh = function () {
-        // AudienceSegmentAnalyticsReportService.setDateRange(todayDate);
-        // updateStatistics($scope, AudienceSegmentAnalyticsReportService);
-      // };
-
-      // $scope.$watch('reportDateRange', function (newRange) {
-      //   if (!newRange) {
-      //     return;
-      //   }
-
-        /*
-           if reportDateRange changes ,fetch statistics of the current day
-        */
-        // AudienceSegmentAnalyticsReportService.setDateRange(todayDate);
-
-        // updateStatistics($scope, AudienceSegmentAnalyticsReportService);
-      // });
-
       $scope.createAudiencePartition = function (type) {
         $location.path(Session.getWorkspacePrefixUrl() + "/datamart/partitions/" + type);
       };
@@ -62,23 +38,6 @@ define(['./module'], function (module) {
         $location.path(Session.getWorkspacePrefixUrl() + "/datamart/partitions/" + audiencePartition.type + "/" + audiencePartition.id);
       };
 
-      // $scope.deleteAudiencePartition = function (audiencePartition, $event) {
-      //   if ($event) {
-      //     $event.preventDefault();
-      //     $event.stopPropagation();
-      //   }
-
-      //   var newScope = $scope.$new(true);
-      //   newScope.audiencePartition = audiencePartition;
-      //   $uibModal.open({
-      //     templateUrl: 'src/core/datamart/partitions/delete.html',
-      //     scope : newScope,
-      //     backdrop : 'static',
-      //     controller: 'core/datamart/partitions/DeleteController'
-      //   });
-
-      //   return false;
-      // };
     }
   ]);
 
--- app/src/core/datamart/partitions/ViewOneController.js
+++ app/src/core/datamart/partitions/ViewOneController.js
@@ -5,11 +5,36 @@ define(['./module'], function (module) {
 
   module.controller('core/datamart/partitions/ViewOneController', [
     '$scope', 'Restangular', 'core/common/auth/Session', '$stateParams', '$q',
-    function ($scope, Restangular, Session, $stateParams, $q) {
+    'core/datamart/segments/report/AudienceSegmentAnalyticsReportService', 'moment', 'lodash',
+    function ($scope, Restangular, Session, $stateParams, $q, AudienceSegmentAnalyticsReportService, moment, _) {
       var partitionId = $stateParams.partition_id;
       var datamartId = Session.getCurrentDatamartId();
 
       var segments = [];
+
+      var todayDate = { startDate: moment(), endDate: moment() };
+      AudienceSegmentAnalyticsReportService.setDateRange(todayDate);
+
+      $scope.hasStats = function (segmentId) {
+        var found = _.find($scope.stats, function (s) {
+          return s.id === segmentId;
+        });
+        return found.usersCount;
+      };
+
+      $scope.getUsersCount = function(segmentId){
+        var found = _.find($scope.stats, function(s){
+          return s.id === segmentId;
+        });
+        return found.usersCount;
+      };
+
+      $scope.getUsersCountPercent = function (segmentId) {
+        var found = _.find($scope.stats, function (s) {
+          return s.id === segmentId;
+        });
+        return found.percentage;
+      };
 
       $q.all([
         Restangular.one('audience_partitions', partitionId).get(),
@@ -20,6 +45,29 @@ define(['./module'], function (module) {
 
         $scope.partition = partition;
         $scope.segments = segments;
+        $scope.stats = [];
+
+        if (segments.length > 0){
+          AudienceSegmentAnalyticsReportService.audienceSegmentsForPartition(partitionId).then(function(report){            
+            var totalUsers = report.getRows().map(function(r){ 
+              return r[1];
+            }).reduce(function(a,b){ return (a + b);});
+
+            $scope.stats = segments.map(function(s){
+              var result = { id: s.id };
+            
+              var rows = report.getRow(s.id);              
+              if (rows && rows.length > 0) {
+                var value = report.getRow(s.id)[0].value;
+                result.usersCount = value;
+                result.percentage = ((value / totalUsers) * 100).toFixed(2);
+              }
+              
+              return result;
+            });
+
+          });
+        }
 
       });
 
