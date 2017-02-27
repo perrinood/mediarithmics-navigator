93b844032ad9f43e2d5b360e14ac9b925c68d7a9
Paul Vigneau <pvigneau@mediarithmics.com>
2017-02-27 17:39:45 +0100

    Removed useless config files

--- config/app-configuration.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-var MCS_CONSTANTS = {
-  NAVIGATOR_URL: 'http://navigator.mediarithmics.com',
-  API_URL: 'http://api.mediarithmics.com',
-  PRIVATE_API_URL: 'http://admin-api.mediarithmics.com:8090'
-};
-
-window.MCS_CONSTANTS = MCS_CONSTANTS;
--- config/app-configuration.template.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-var MCS_CONSTANTS = {
-  NAVIGATOR_URL: 'http://navigator.mediarithmics.local',
-  API_URL: 'http://api.mediarithmics.local',
-  PRIVATE_API_URL: 'http://admin-api.mediarithmics.local:8090'
-};
-
-window.MCS_CONSTANTS = MCS_CONSTANTS;
